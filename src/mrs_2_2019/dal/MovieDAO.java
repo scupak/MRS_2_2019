c0e5e78899b68beaa4b88709ec3bc2c970906c79
Stegger <stegger.peter@gmail.com>
2019-11-07 10:42:12 +0100

    Added get all movies method
    Though it's poorly implemented.

---
+++ src/mrs_2_2019/dal/MovieDAO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mrs_2_2019.dal;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import mrs_2_2019.be.Movie;
+
+/**
+ *
+ * @author pgn
+ */
+public class MovieDAO
+{
+
+    private static final String MOVIE_SOURCE = "data/movie_titles.txt";
+
+    public List<Movie> getAllMovies() throws FileNotFoundException, IOException
+    {
+        File file = new File(MOVIE_SOURCE);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        while (true)
+        {
+            String aLineOfText = br.readLine();
+            if (aLineOfText == null)
+            {
+                break;
+            } else
+            {
+                System.out.println(aLineOfText);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        MovieDAO movieDao = new MovieDAO();
+        movieDao.getAllMovies();
+    }
+
+}
